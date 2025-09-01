a5e694bc4146ca6bebac642d2f800f67dd4ac436
pjw <jwp000708@google.com>
2025-09-01 15:40:36 +0900

    day1 basic1.ts

---
+++ day1/basic.ts
@@ -0,0 +1,18 @@
+// 변수
+let age: number = 27;
+let name: string = "정우";
+let isActive: boolean = true;
+
+// 배열
+let numbers: number[] = [1, 2, 3];
+let names: Array<string> = ["철수", "영희"];
+
+// 튜플
+let user: [string, number] = ["Alice", 25];
+
+//함수
+function add(a: number, b: number): number {
+  return a + b;
+}
+
+console.log(add(5, 7));
